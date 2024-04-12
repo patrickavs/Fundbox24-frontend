314ced126e5d1004ad862b71c34cdc287dc2b3a8
Tim <timwagner997@gmail.com>
2024-04-12 22:23:47 +0200

    added dummy circle

--- App.tsx
+++ App.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import MapView from 'react-native-maps';
+import MapView, {LatLng} from 'react-native-maps';
 import {StyleSheet} from 'react-native';
+import {Circle} from 'react-native-maps';
 
 function App(): React.JSX.Element {
+  const hofUniversityPosition: LatLng = {
+    latitude: 50.3254386,
+    longitude: 11.9384522,
+  };
+
   return (
     <MapView
       style={styles.map}
       initialRegion={{
-        latitude: 50.3254386,
-        longitude: 11.9384522,
+        ...hofUniversityPosition,
         latitudeDelta: 0.03,
         longitudeDelta: 0.03,
-      }}
-    />
+      }}>
+      <Circle
+        center={hofUniversityPosition}
+        radius={700}
+        fillColor="rgba(245, 39, 145, 0.3)"
+        strokeWidth={0}
+      />
+    </MapView>
   );
 }
 
