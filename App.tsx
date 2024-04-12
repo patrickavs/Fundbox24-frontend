92c4b4d6ee0b0bb9d3bef7f78788ccbab0b39301
Tim <timwagner997@gmail.com>
2024-04-12 22:16:11 +0200

    added example map

--- App.tsx
+++ App.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import {WebView} from 'react-native-webview';
+import MapView from 'react-native-maps';
+import {StyleSheet} from 'react-native';
 
 function App(): React.JSX.Element {
-  return <WebView source={{html: '<p>Hallo Welt</p>'}} />;
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 50.3254386,
+        longitude: 11.9384522,
+        latitudeDelta: 0.03,
+        longitudeDelta: 0.03,
+      }}
+    />
+  );
 }
+
+const styles = StyleSheet.create({
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 export default App;
