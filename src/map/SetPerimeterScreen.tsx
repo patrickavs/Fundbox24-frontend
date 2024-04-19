e7216dab3cf46fc6203d4d19b892282f4a8cfb5b
Tim <timwagner997@gmail.com>
2024-04-19 13:58:02 +0200

    user can now change the position of the perimeter

--- src/map/SetPerimeterScreen.tsx
+++ src/map/SetPerimeterScreen.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import MapView, {LatLng} from 'react-native-maps';
+import MapView, {Circle, LatLng} from 'react-native-maps';
 import {Button, StyleSheet, Text, View} from 'react-native';
-import {Circle} from 'react-native-maps';
 import {Slider} from '@miblanchard/react-native-slider';
 
 const minRadius: number = 300;
 const maxRadius: number = 3000;
 
+const initialPosition: LatLng = {
+  latitude: 50.3254386,
+  longitude: 11.9384522,
+};
+
 const numberFormat = Intl.NumberFormat('de-DE', {maximumFractionDigits: 1});
 
 function SetPerimeterScreen(): React.JSX.Element {
+  const [position, setPosition] = React.useState<LatLng>(initialPosition);
   const [radius, setRadius] = React.useState<number>(minRadius);
-
-  const hofUniversityPosition: LatLng = {
-    latitude: 50.3254386,
-    longitude: 11.9384522,
-  };
 
   function changeRadius(sliderValue: number) {
     const newRadius = minRadius + sliderValue * (maxRadius - minRadius);
@@ -27,18 +27,23 @@ function SetPerimeterScreen(): React.JSX.Element {
     return `${numberFormat.format(radiusInKM)} km`;
   }
 
+  function changePosition(newPosition: LatLng) {
+    setPosition(newPosition);
+  }
+
   return (
     // TODO: tap to change position of perimeter
     <View style={styles.absoluteFill}>
       <MapView
+        onPress={event => changePosition(event.nativeEvent.coordinate)}
         style={styles.absoluteFill}
         initialRegion={{
-          ...hofUniversityPosition,
+          ...position,
           latitudeDelta: 0.03,
           longitudeDelta: 0.03,
         }}>
         <Circle
-          center={hofUniversityPosition}
+          center={position}
           radius={radius}
           fillColor="rgba(245, 39, 145, 0.3)"
           strokeWidth={0}
