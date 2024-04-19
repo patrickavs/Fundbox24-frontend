54a32268f3190efd72c92b658d4969b48f03288d
Tim <timwagner997@gmail.com>
2024-04-19 13:01:10 +0200

    wip: added state to perimeter radius

--- src/map/SetPerimeterScreen.tsx
+++ src/map/SetPerimeterScreen.tsx
@@ -4,13 +4,28 @@ import {Button, StyleSheet, Text, View} from 'react-native';
 import {Circle} from 'react-native-maps';
 import {Slider} from '@miblanchard/react-native-slider';
 
+const minRadius: number = 300;
+const maxRadius: number = 3000;
+
 function SetPerimeterScreen(): React.JSX.Element {
+  const [radius, setRadius] = React.useState<number>(minRadius);
+
   const hofUniversityPosition: LatLng = {
     latitude: 50.3254386,
     longitude: 11.9384522,
   };
 
+  function changeRadius(sliderValue: number) {
+    const newRadius = minRadius + sliderValue * (maxRadius - minRadius);
+    setRadius(newRadius);
+  }
+
+  function getFormattedDiameter(): string {
+    return `${radius} m`;
+  }
+
   return (
+    // TODO: tap to change position of perimeter
     <View style={styles.absoluteFill}>
       <MapView
         style={styles.absoluteFill}
@@ -21,13 +36,14 @@ function SetPerimeterScreen(): React.JSX.Element {
         }}>
         <Circle
           center={hofUniversityPosition}
-          radius={700}
+          radius={radius}
           fillColor="rgba(245, 39, 145, 0.3)"
           strokeWidth={0}
         />
       </MapView>
       <View style={styles.controls}>
-        <Slider />
+        <Text style={styles.diameterText}>{getFormattedDiameter()}</Text>
+        <Slider value={0.5} onValueChange={value => changeRadius(value[0])} />
         <Button title="Gegenstand melden" />
       </View>
     </View>
@@ -35,14 +51,17 @@ function SetPerimeterScreen(): React.JSX.Element {
 }
 
 const styles = StyleSheet.create({
+  diameterText: {
+    textAlign: 'center',
+  },
   absoluteFill: {
     ...StyleSheet.absoluteFillObject,
   },
   controls: {
     marginTop: 'auto',
-    padding: 20,
+    padding: 30,
     backgroundColor: 'white',
-    alignItems: 'center',
+    alignItems: 'stretch',
     display: 'flex',
     flexDirection: 'column',
     gap: 20,
