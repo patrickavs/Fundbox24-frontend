5670c43cb7dbcebaae6db2134e98183d628e0bbc
Lukas Drescher <lukas.drescher@hof-university.de>
2024-05-15 15:51:14 +0200

    Created the last hook

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,3 +3,4 @@ export const CHAT_URL = `${BASE_URL}/chat`;
 export const MESSAGE_URL = (id: string) => `${CHAT_URL}/${id}/message`;
 export const FOUNDREPORT_URL = (id?: string) =>
   `${BASE_URL}/found-report/${id}`;
+export const LOSTREPORT_URL = (id?: string) => `${BASE_URL}/lost-report/${id}`;
