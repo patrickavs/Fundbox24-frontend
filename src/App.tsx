8fcff155f4aba3a1e9022d9613855b62176fef08
Tim <timwagner997@gmail.com>
2024-05-20 17:59:20 +0200

    ignore warning for React Native Dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,14 @@ import {Provider} from 'react-redux';
 import store from './redux/store.ts';
 import Tabbar from './components/tabbar/Tabbar.tsx';
 import {AuthTheme} from './constants/theme.ts';
+import {LogBox} from 'react-native';
 
 function App(): React.JSX.Element {
+  // ignore warning for React Native Dropdown
+  LogBox.ignoreLogs([
+    'TextInputComponent: Support for defaultProps will be removed',
+  ]);
+
   return (
     <Provider store={store}>
       <NavigationContainer theme={AuthTheme}>
