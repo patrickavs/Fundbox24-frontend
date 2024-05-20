027c49ab82b7631b298556718ff7d231dcd604a4
Tim <timwagner997@gmail.com>
2024-05-20 16:42:16 +0200

    header should be outside of scroll container

--- src/pages/lost/LostReportScreen.tsx
+++ src/pages/lost/LostReportScreen.tsx
@@ -25,68 +25,71 @@ function LostReportScreen(): React.JSX.Element {
   const {lostReports} = useLostReports();
 
   return (
-    <ScrollView>
+    <View>
       <CustomHeader
         backgroundColor={LostReportTheme.colors.secondaryAccent}
         title={'Suchanzeigen'}
       />
-
-      <SearchBar
-        style={styles.searchBar}
-        onChangeText={text => {
-          console.log('Benutzer sucht nach: ' + text);
-        }}
-      />
-
-      <View style={styles.dropdowns}>
-        <Dropdown
-          placeholder="Sortieren"
-          items={[
-            {label: 'Alphabetisch', value: 'alphabetical'},
-            {label: 'Zuletzt gesehen', value: 'last seen'},
-            {label: 'Entfernung', value: 'distance'},
-          ]}
-          onChange={item => {
-            console.log('Benutzer hat sortiert nach: ' + item.value);
+      <ScrollView style={styles.scrollContainer}>
+        <SearchBar
+          onChangeText={text => {
+            console.log('Benutzer sucht nach: ' + text);
           }}
         />
-        <Dropdown
-          placeholder="Filtern"
-          items={[
-            {label: 'Nur mein Heimatumkreis', value: 'in my region'},
-            {label: 'Nur heute', value: 'only today'},
-          ]}
-          onChange={item => {
-            console.log('Benutzer hat gefiltert nach: ' + item.value);
-          }}
-        />
-      </View>
-      <View>
-        {lostReports.map((report, idx) => {
-          // Problem, aktuell wird kein key verwendet
-          if (idx % 2 === 0) {
-            return (
-              <>
-                <ListItem report={report} />
-                <Text>Wusstest du schon, dass...</Text>
-              </>
-            );
-          }
-          return <ListItem key={report.id} report={report} />;
-        })}
-      </View>
-    </ScrollView>
+        <View style={styles.dropdownContainer}>
+          <Dropdown
+            placeholder="Sortieren"
+            items={[
+              {label: 'Alphabetisch', value: 'alphabetical'},
+              {label: 'Zuletzt gesehen', value: 'last seen'},
+              {label: 'Entfernung', value: 'distance'},
+            ]}
+            onChange={item => {
+              console.log('Benutzer hat sortiert nach: ' + item.value);
+            }}
+          />
+          <Dropdown
+            placeholder="Filtern"
+            items={[
+              {label: 'Nur mein Heimatumkreis', value: 'in my region'},
+              {label: 'Nur heute', value: 'only today'},
+            ]}
+            onChange={item => {
+              console.log('Benutzer hat gefiltert nach: ' + item.value);
+            }}
+          />
+        </View>
+
+        <Text>Gesucht in deinem Umkreis</Text>
+        <View>
+          {lostReports.map((report, idx) => {
+            // Problem, aktuell wird kein key verwendet
+            if (idx % 2 === 0) {
+              return (
+                <>
+                  <ListItem report={report} />
+                  <Text>Wusstest du schon, dass...</Text>
+                </>
+              );
+            }
+            return <ListItem key={report.id} report={report} />;
+          })}
+        </View>
+      </ScrollView>
+    </View>
   );
 }
 
 export default LostReportScreen;
 
 const styles = StyleSheet.create({
-  searchBar: {
-    margin: 30,
+  scrollContainer: {
+    margin: 20,
   },
-  dropdowns: {
+  dropdownContainer: {
+    marginTop: 20,
     display: 'flex',
     flexDirection: 'row',
+    gap: 20,
   },
 });
