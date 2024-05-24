7ae325f61357e9d283a036ed6e252387d7fc1133
patrickavs <patrick-rc-pfeil@gmx.de>
2024-05-24 11:18:30 +0200

    fixed FoundreportURL and LostreportURL

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 export const BASE_URL = 'http://10.0.2.2:8080';
 export const CHAT_URL = `${BASE_URL}/chat`;
 export const MESSAGE_URL = (id: string) => `${CHAT_URL}/${id}/message`;
-export const FOUNDREPORT_URL = (id?: string) => `${BASE_URL}/report/found${id}`;
-export const LOSTREPORT_URL = (id?: string) => `${BASE_URL}/report/lost${id}`;
+export const FOUNDREPORT_URL = (id?: string) =>
+  `${BASE_URL}/report/found/${id}`;
+export const LOSTREPORT_URL = (id?: string) => `${BASE_URL}/report/lost/${id}`;
 
 export const ALL_LOST_REPORTS_URL = `${BASE_URL}/report/lost`;
 export const ALL_FOUND_REPORTS_URL = `${BASE_URL}/report/found`;
