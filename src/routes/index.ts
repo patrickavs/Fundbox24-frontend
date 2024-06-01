42e88fabc6004778f5551224750a0b9eb678725c
Tim <timwagner997@gmail.com>
2024-06-01 20:09:59 +0200

    added url for login

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,3 +7,5 @@ export const LOSTREPORT_URL = (id?: string) => `${BASE_URL}/report/lost/${id}`;
 
 export const ALL_LOST_REPORTS_URL = `${BASE_URL}/report/lost`;
 export const ALL_FOUND_REPORTS_URL = `${BASE_URL}/report/found`;
+
+export const LOGIN_URL = `${BASE_URL}/login`;
