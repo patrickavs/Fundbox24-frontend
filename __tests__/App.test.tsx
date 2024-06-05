7734ea81f99acc84773ed5741672f7b4643308c0
Lukas Drescher <145000859+BequemeDecke@users.noreply.github.com>
2024-06-05 16:11:39 +0200

    First tests run with AsyncStorage Mockup

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,16 +1,19 @@
-import renderer from 'react-test-renderer';
 import App from '../src/App';
 
 // Note: import explicitly to use the types shipped with jest.
-import {expect, it} from '@jest/globals';
-import {render} from '@testing-library/react-native';
+import { expect, it, jest, describe } from '@jest/globals';
+import { render, screen } from '@testing-library/react-native';
 
-it('renders App correctly', () => {
-  render(<App />);
+describe('App', () => {
+  it('should display ""', () => {
+    render(<App />);
+
+    expect(screen.getByText('')).toBeTruthy();
+  });
 });
 
-it('renderer App has 2 child', () => {
-  const tree = renderer.create(<App />).toJSON();
-  //console.log(JSON.stringify(tree));
-  expect(tree.children.length).toBe(2);
-});
+// it('renderer App has 2 child', () => {
+//   const tree = renderer.create(<App />).toJSON();
+//   //console.log(JSON.stringify(tree));
+//   expect(tree.children.length).toBe(2);
+// });
--- __tests__/pages/home/StartScreen.test.tsx
+++ __tests__/pages/home/StartScreen.test.tsx
@@ -1,59 +1,64 @@
 import 'react-native';
 import React from 'react';
 import { render, screen } from '@testing-library/react-native';
-import { expect, it, jest } from '@jest/globals';
+import { expect, it, jest, describe } from '@jest/globals';
 import StartScreen from '../../../src/pages/home/StartScreen.tsx';
 import { UserProvider } from '../../../src/hooks/useUser.tsx';
 import { LostReportProvider } from '../../../src/hooks/useLostReports.tsx';
 import { FoundReportProvider } from '../../../src/hooks/useFoundReports.tsx';
 import { ChatProvider } from '../../../src/hooks/useChat.tsx';
 import { LostReport } from '../../../src/types/report-lost.ts';
+import { act } from 'react-test-renderer';
 
-it('renders find text "Du bist nicht angemeldet"', () => {
+describe('StartScreen', () => {
+  it('renders find text "Du bist nicht angemeldet"', () => {
 
-  const fakeLostReports: LostReport[] = [
-    {
-      id: '1',
-      title: 'Schlüssel',
-      description: 'Ein Schlüsselbund mit 3 Schlüsseln',
-      lastSeenDate: new Date(Date.now()).toLocaleTimeString(),
-      lastSeenLocation: {
-        latitude: 53.551086,
-        longitude: 9.993682
-      },
-      lostLocation: {
-        latitude: 53.551086,
-        longitude: 9.993682
-      },
-      lostRadius: 100,
-      category: {
+    const fakeLostReports: LostReport[] = [
+      {
         id: '1',
-        value: "",
         title: 'Schlüssel',
-        image: ""
-      },
-      placeOfDiscovery: 'Hamburg',
-      placeOfDelivery: 'Hamburg',
-      myChats: []
-    }
-  ]
+        description: 'Ein Schlüsselbund mit 3 Schlüsseln',
+        lastSeenDate: new Date(Date.now()).toLocaleTimeString(),
+        lastSeenLocation: {
+          latitude: 53.551086,
+          longitude: 9.993682
+        },
+        lostLocation: {
+          latitude: 53.551086,
+          longitude: 9.993682
+        },
+        lostRadius: 100,
+        category: {
+          id: '1',
+          value: "",
+          title: 'Schlüssel',
+          image: ""
+        },
+        placeOfDiscovery: 'Hamburg',
+        placeOfDelivery: 'Hamburg',
+        myChats: []
+      }
+    ]
 
-  jest.spyOn(global, 'fetch').mockImplementationOnce(() =>
-    Promise.resolve({
-      json: () => Promise.resolve(fakeLostReports),
-    }) as Promise<Response>
-  );
+    jest.spyOn(global, 'fetch').mockImplementationOnce(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(fakeLostReports),
+      }) as Promise<Response>
+    );
 
-  render(
-    <UserProvider>
-      <LostReportProvider>
-        <FoundReportProvider>
-          <ChatProvider>
-            <StartScreen />;
-          </ChatProvider>
-        </FoundReportProvider>
-      </LostReportProvider>
-    </UserProvider>
-  );
-  expect(screen.getByText('Willkommen, blauerwal24').props.children).toBeTruthy();
+    render(
+      <UserProvider>
+        <LostReportProvider>
+          <FoundReportProvider>
+            <ChatProvider>
+              <StartScreen />;
+            </ChatProvider>
+          </FoundReportProvider>
+        </LostReportProvider>
+      </UserProvider>
+    );
+
+    expect(screen.getByText('Willkommen, blauerwal24').props.children).toBeTruthy();
+  });
 });
+
