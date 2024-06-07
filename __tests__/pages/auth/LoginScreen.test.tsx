5128efed618db0cbece511dfdb954bcd6610bc42
Tim <timwagner997@gmail.com>
2024-06-07 13:31:18 +0200

    simulate press on login button

--- __tests__/pages/auth/LoginScreen.test.tsx
+++ __tests__/pages/auth/LoginScreen.test.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
-import {describe, it} from '@jest/globals';
-import {render} from '@testing-library/react-native';
+import {describe, it, jest} from '@jest/globals';
+import {fireEvent, render, screen} from '@testing-library/react-native';
 import LoginScreen from '../../../src/components/auth/LoginScreen.tsx';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {UserProvider} from '../../../src/hooks/useUser.tsx';
 
 describe('LoginScreen', () => {
-  it('can login with correct credentials', () => {
-    render(<LoginScreen />);
+  // Mock the fetch function
+  jest.spyOn(global, 'fetch').mockImplementation(() =>
+    Promise.resolve({
+      json: () => Promise.resolve({}),
+      status: 200,
+      ok: true,
+    } as Response),
+  );
+
+  it('can login with correct credentials', async () => {
+    const AuthStackNavigator = createNativeStackNavigator();
+
+    render(
+      <UserProvider>
+        <NavigationContainer>
+          <AuthStackNavigator.Navigator>
+            <AuthStackNavigator.Screen
+              name="Login"
+              component={LoginScreen}
+              options={{title: 'Login', headerShown: false}}
+            />
+          </AuthStackNavigator.Navigator>
+        </NavigationContainer>
+      </UserProvider>,
+    );
+
+    // Press login button
+    const loginButton = await screen.findByTestId('LoginButton');
+    fireEvent.press(loginButton);
   });
 });
