e551f9581264c3d9908f57787f84eaa4b08756bc
awaas <alexandra.waas@hof-university.de>
2024-07-07 12:39:20 +0200

    fixed test

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
     'RNTester/e2e',
   ],
   transformIgnorePatterns: [
-    'node_modules/(?!@react-native|react-native|@expo-modules-core|expo-modules-core|expo|@expo|@react-navigation|react-navigation|@babel|babel|@jest|jest|react-redux|@react-redux)',
+    'node_modules/(?!@react-native|react-native|@expo-modules-core|expo-modules-core|expo|@expo|@react-navigation|react-navigation|@babel|babel|@jest|jest|react-redux|@react-redux|(@miblanchard/react-native-slider)/)',
   ],
   fakeTimers: {
     enableGlobally: true,
